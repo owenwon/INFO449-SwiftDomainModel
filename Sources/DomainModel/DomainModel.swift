1f33047c6551f09c337a3d01f7e12e878e578ee0
Owen Wong <owenwong@uw.edu>
2026-01-21 19:25:58 -0800

    Passed all money tests

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,6 +9,55 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    var amount: Int
+    var currency: String
+
+    func convert (_ currencyName: String) -> Money {
+        var tempAmount = Double(self.amount)
+
+        // convert the current amount to USD
+        switch currency {
+        case "USD":
+            break
+        case "GBP":
+            tempAmount = Double(self.amount) / 0.5
+        case "EUR":
+            tempAmount = Double(self.amount) / 1.5
+        case "CAN":
+            tempAmount = Double(self.amount) / 1.25
+        default:
+            print("Currency not available")
+        }
+
+        // convert the current amount from USD to currencyName
+        switch currencyName {
+        case "USD":
+            break
+        case "GBP":
+            tempAmount = tempAmount * 0.5
+        case "EUR":
+            tempAmount = tempAmount * 1.5
+        case "CAN":
+            tempAmount = tempAmount * 1.25
+        default:
+            print("Currency not available")
+        }
+
+        let finalAmount = Int(tempAmount)
+        return Money(amount: finalAmount, currency: currencyName)
+    }
+
+    func add ( other: Money) -> Money {
+        let convertedSelf = self.convert(other.currency)
+        let newAmount = convertedSelf.amount + other.amount
+        return Money(amount: newAmount, currency: convertedSelf.currency)
+    }
+
+    func subtract (_ other: Money) -> Money {
+        let convertedSelf = self.convert(other.currency)
+        let newAmount = convertedSelf.amount - other.amount
+        return Money(amount: newAmount, currency: convertedSelf.currency)
+    }
 }
 
 ////////////////////////////////////
